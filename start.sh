882106ad42255b71ebbd4e7be2452afb14158d7f
Aravindhan-a-p <118278879+Aravindhan-a-p@users.noreply.github.com>
2023-06-11 22:54:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ sudo a2enmod proxy
 sudo a2enmod proxy_http
 
 cp ./gammaz.hm.conf /etc/apache2/sites-available/
-sudo ln -s /etc/apache2/sites-available/gamma-z.hm.conf /etc/apache2/sites-enabled/gamma-z.hm.conf
+sudo ln -s /etc/apache2/sites-available/gamma-z.hm.conf /etc/apache2/sites-enabled/gammaz.hm.conf
 
 echo "127.0.0.1 gammaz.hm" | sudo tee -a /etc/hosts > /dev/null
 sudo systemctl restart apache2
